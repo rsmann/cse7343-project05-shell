372e6c699eb2a71bec4625f1f6bffc0bea000f89
Scott Mann <ssmann@smu.edu>
2013-10-21 23:02:23 -0500

    Total mess of attempts at parsing that I'm takin gout, but may want back later...

--- shell.c
+++ shell.c
@@ -1,19 +1,22 @@
-// Your Name
-// Description.
-
+// Scott Mann - CSE 7343 - 10/20/2013
+// A simple Unix shell.
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
-void copy(char *filename1, char *filename2);
+void copy(char *source, char *destination);
+void delete(char* filename);
 void displayError(char* command);
+void execute(char* command);
 bool isCopy(char* command);
 bool isDelete(char* command);
-bool isExecute(char* command);
+bool isExecutable(char* command);
 bool isExit(char* command);
 bool isType(char* command);
-void type(char* command);
+void type(char* filename);
+
 
 int main(int argc, char **argv) {
 	// TODO
@@ -32,43 +35,74 @@ int main(int argc, char **argv) {
 			printf("Exiting... Thanks for using SMUSh - Scott's Shell.\n");
 			break;
 		}
-		else if (isCopy(command))
-		{
 
+		// The command wasn't exit, go ahead and parse the arguments once
+		// for future use.
+		int count = 0;
+		char input[100] = {0};
+		char result[100][32];
+		strcpy(input, command);
+		count = split2(input, result);
+
+		int i = 0;
+		for (i=0; i<count; i++) {
+			printf("%s+", result[i]);
 		}
-		else if (isDelete(command))
-		{
 
+		if (isCopy(result[0]))
+		{
+			copy(result[1], result[2]);
 		}
-		else if (isType(command))
+		else if (isDelete(result[0]))
 		{
-
+			delete(result[1]);
+		}
+		else if (isType(result[0]))
+		{
+			type(result[1]);
+		}
+		else if (isExecutable(command))
+		{
+			execute(command);
 		}
 		else
 		{
-			if (isExecute(command))
-			{
-
-			}
-			else
-			{
-				displayError(command);
-			}
+			displayError(command);
 		}
-
-		// If command is type,
-		// type(command)
-
-		// If command is copy
-		// copy(file1, file2)
 	}
 
 	return 0;
 }
 
-void copy(char *filename1, char *filename2)
+int split2(char* str, char results[256][32])
 {
+	//const char line[] = "2004/12/03 12:01:59;info1;info2;info3";
+	const char *ptr = str; //line
+	char field [ 32 ];
+	int n;
+	int i = 0;
+	while ( sscanf(ptr, "%31[^ ]%n", field, &n) == 1 )
+	{
+		printf("field = \"%s\"\n", field);
+		strcpy(results[i++], field);
+		ptr += n; /* advance the pointer by the number of characters read */
+		if ( *ptr != ' ' )
+		{
+			break; /* didn't find an expected delimiter, done? */
+		}
+		++ptr; /* skip the delimiter */
+	}
+	return i;
+}
 
+void copy(char *source, char *destination)
+{
+	printf("Copying...\n");
+}
+
+void delete(char* filename)
+{
+	printf("Deleting...\n");
 }
 
 void displayError(char* command)
@@ -76,17 +110,22 @@ void displayError(char* command)
 	printf("Command not found: %s\n", command);
 }
 
+void execute(char* command)
+{
+	printf("Executing...\n");
+}
+
 bool isCopy(char* command)
 {
-	return false;
+	return !strcmp(command, "copy");
 }
 
 bool isDelete(char* command)
 {
-	return false;
+	return !strcmp(command, "delete");
 }
 
-bool isExecute(char* command)
+bool isExecutable(char* command)
 {
 	return false;
 }
@@ -98,14 +137,32 @@ bool isExit(char* command)
 
 bool isType(char* command)
 {
-	return false;
+	return !strcmp(command, "type");
 }
 
-void type(char *command)
+void type(char *filename)
 {
-	// open the file
-	char contents[1000];
+	printf("Opening %s for output...", filename);
 
-	// Read the file
-	// Output the file
+	char currentChar = 0;
+	FILE *handle;
+
+	// Open the file
+	handle = fopen(filename, "r");
+
+	// If we didn't get a valid handle back, an error occurred
+	if(handle == NULL)
+	{
+		perror("Error while opening the file.\n");
+		return;
+	}
+
+	// Read every character and print it to the screen
+	while((currentChar = fgetc(handle)) != EOF)
+	{
+		printf("%c", currentChar);
+	}
+
+	// Close the file cleanly
+	fclose(handle);
 }
